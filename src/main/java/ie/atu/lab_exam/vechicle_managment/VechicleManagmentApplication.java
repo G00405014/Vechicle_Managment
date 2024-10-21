3457f49953d121c487819547a5e4b34f9ee03962
G00405014@atu.ie <G00405014@atu.ie>
2024-10-21 12:11:37 +0100

    Added dependencies

---
+++ src/main/java/ie/atu/lab_exam/vechicle_managment/VechicleManagmentApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_exam.vechicle_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VechicleManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VechicleManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab_exam/vechicle_managment/VechicleManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_exam.vechicle_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VechicleManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
